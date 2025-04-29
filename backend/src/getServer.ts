dec273f82fcbad51cda5351f8a56b3383e5e1cd1
fixten <fixten@ya.ru>
2025-04-29 16:20:17 +0000

    setup entry functions

---
+++ backend/src/getServer.ts
@@ -0,0 +1,24 @@
+import { dirname, resolve } from "path";
+import { fileURLToPath } from "url";
+
+import bodyParser from "body-parser";
+import { config } from "dotenv";
+import express from "express";
+
+import dbConnection from "./db/dbConnection.js";
+import settingsRouter from "./features/settings/settings.route.js";
+
+export default function getServer() {
+  const __dirname = dirname(fileURLToPath(import.meta.url));
+  config({ path: resolve(__dirname, "../../.env") });
+
+  const app = express();
+  dbConnection.connect();
+
+  app.use(bodyParser.json());
+  app.get("/", (req, res) => {
+    res.send("Hello World!");
+  });
+  app.use("/settings", settingsRouter);
+  return app;
+}
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,29 +1,13 @@
-import { dirname, resolve } from "path";
-import { fileURLToPath } from "url";
+import getServer from "./getServer.js";
 
-import bodyParser from "body-parser";
-import { config } from "dotenv";
-import express from "express";
+function startApp() {
+  const server = getServer();
+  const { BACKEND_PORT } = process.env;
+  if (BACKEND_PORT) {
+    server.listen(BACKEND_PORT, () => {
+      console.log(`Sklad app listening on port ${BACKEND_PORT}`);
+    });
+  } else throw new Error("Environment variables are not set");
+}
 
-import settingsRouter from "features/settings/settings.route.js";
-
-import dbConnection from "./db/dbConnection.js";
-
-const __dirname = dirname(fileURLToPath(import.meta.url));
-config({ path: resolve(__dirname, "../../.env") });
-dbConnection.connect();
-const app = express();
-
-const { BACKEND_PORT } = process.env;
-
-if (BACKEND_PORT) {
-  app.use(bodyParser.json());
-  app.get("/", (req, res) => {
-    res.send("Hello World!");
-  });
-  app.use("/settings", settingsRouter);
-
-  app.listen(BACKEND_PORT, () => {
-    console.log(`Sklad app listening on port ${BACKEND_PORT}`);
-  });
-} else throw new Error("Environment variables are not set");
+export default startApp();
