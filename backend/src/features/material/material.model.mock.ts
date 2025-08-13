c1ebeec45bb8ea01309b76d5caf2da43218cba52
fixten <fixten@ya.ru>
2025-08-13 14:59:30 +0300

    material model mock

---
+++ backend/src/features/material/material.model.mock.ts
@@ -0,0 +1,14 @@
+import { ObjectId } from "mongodb";
+import { MaterialModel, VariantModel } from "./material.model.js";
+
+export const variantModelMock: VariantModel = {
+  variant: "variant",
+  photo_url: "url",
+};
+
+export const materialModelMock: MaterialModel = {
+  name: "name",
+  description: "string",
+  materialType: new ObjectId(),
+  variants: [],
+};
